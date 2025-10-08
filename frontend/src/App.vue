a5263e4aa22e0817dd210bf07a968d53da4744fd
Maikol Amaro <maikoldepaula@gmail.com>
2025-10-07 23:29:25 -0300

    feat: add initial config this project

---
+++ frontend/src/App.vue
@@ -0,0 +1,57 @@
+
+<template>
+   <Dialog>
+    <DialogTrigger>
+      Edita seu perfil
+    </DialogTrigger>
+    <DialogContent>
+      <!-- <DialogHeader>
+        <DialogTitle>Edit profile</DialogTitle>
+        <DialogDescription>
+          Make changes to your profile here. Click save when you're done.
+        </DialogDescription>
+      </DialogHeader>
+
+      <DialogFooter>
+        Save changes
+      </DialogFooter> -->
+    </DialogContent>
+  </Dialog>
+
+<Button class="ml-5 cursor-pointer" size="lg">
+  teste
+</Button>
+
+
+</template>
+
+<script setup lang="ts">
+import Button from '@/components/ui/button/Button.vue';
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from '@/components/ui/dialog'
+
+</script>
+
+
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
